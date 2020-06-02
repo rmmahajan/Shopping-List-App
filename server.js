110de70337558f9fe7ff633233c883d035962e45
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-02 23:23:21 +0530

    v2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //BodyParser middleware
@@ -14,6 +16,10 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
         .then(() => console.log('Connection Establish'))
         .catch(err => console.log(err));
+
+
+//Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
