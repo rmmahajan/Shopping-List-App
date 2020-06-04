3ca47d6fb6dda34be198798fd15c46427c78c7ad
rmmahajan <manvi.mahajan20@gmail.com>
2020-06-04 20:07:19 +0530

    v4

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
